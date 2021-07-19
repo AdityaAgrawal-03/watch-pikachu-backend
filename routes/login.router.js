218ea25b5ecac0aa20c56479705576c594004bad
Aditya Agrawal <55623652+AdityaAgrawal-03@users.noreply.github.com>
2021-07-19 17:23:01 +0000

    chore: modified the response sent to the client

--- routes/login.router.js
+++ routes/login.router.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const loginRouter = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/user.model");
+const secret = process.env['secret'];
 
 loginRouter.route("/")
   .post(async (req, res) => {
@@ -13,7 +15,8 @@ loginRouter.route("/")
         const validatePassword = await bcrypt.compare(password, user.password);
         console.log({ validatePassword })
         if (validatePassword) {
-          return res.status(200).json({ success: true, message: "user successfully logged in", user })
+          const token = jwt.sign({ userId: user._id }, secret, { expiresIn: "24h" });
+          return res.status(200).json({ success: true, message: "user successfully logged in", user, token })
         } return res.status(400).json({ success: false, message: "Invalid password" })
       } return res.status(401).json({ success: false, message: "Unathorized access, user does not exist!" })
     } catch (error) {
